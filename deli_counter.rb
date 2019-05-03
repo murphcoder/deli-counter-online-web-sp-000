9b6f506abc02d44b68056be45f564d474f87bfbf
Sean Murphy <filmmakersean@fastmail.net>
2019-05-03 17:50:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+
+def line(customers)
+  if customers.count == 0
+    puts "The line is currently empty."
+  else
+    print "The line is currently:"
+    count = 1
+    while customers.count >= count do
+      print " #{count}. #{customers[count - 1]}"
+    end
+  end
+end
+
