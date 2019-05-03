ecaf87f93c86c91ea0f527a35855fc35b7f6c924
Sean Murphy <filmmakersean@fastmail.net>
2019-05-03 17:53:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,6 @@ def line(customers)
   if customers.count == 0
     puts "The line is currently empty."
   else
-    print "The line is currently:"
-    count = 1
-    while customers.count >= count do
-      print " #{count}. #{customers[count - 1]}"
-      count += 1
-    end
-  end
+    status = "The line is currently: "
 end
 
