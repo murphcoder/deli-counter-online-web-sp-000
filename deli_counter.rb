c9fca8a232fdf6e91d8d1720dc3e9da06b244c7d
Sean Murphy <filmmakersean@fastmail.net>
2019-05-03 17:57:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ def line(customers)
   if customers.count == 0
     puts "The line is currently empty."
   else
-    status = "The line is currently: "
+    status = "The line is currently:"
+    customers.each_with_index |name, place| do
+      status << " "
 end
 
